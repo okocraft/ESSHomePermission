4301352fe11ec866de31575021f197b87bfee07f
LazyGon <26216029+LazyGon@users.noreply.github.com>
2023-03-12 09:14:37 +0900

    feat: folia supported

--- src/main/java/net/okocraft/esshomepermission/ESSHomePermissionPlugin.java
+++ src/main/java/net/okocraft/esshomepermission/ESSHomePermissionPlugin.java
@@ -17,38 +17,36 @@ public class ESSHomePermissionPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Listener() {
-
-            @EventHandler
-            public void onUserTeleportHome(UserTeleportHomeEvent event) {
-                if (event.getHomeType() != UserTeleportHomeEvent.HomeType.HOME) {
-                    return;
-                }
-                if (!(event.getUser() instanceof User user)) {
-                    return;
-                }
-
-                List<String> homes = user.getHomes();
-                int index = homes.indexOf(event.getHomeName());
-                int availableIndex = getAvailableHomeLimit(user) - 1;
-                if (index <= availableIndex) {
-                    return;
-                }
-
-                event.setCancelled(true);
-
-                for (int i = 0; i < homes.size(); i++) {
-                    if (i > availableIndex) {
-                        homes.set(i, ChatColor.STRIKETHROUGH + homes.get(i));
-                    }
-                }
-                user.sendMessage(I18n.tl("homes", StringUtil.joinList(homes), homes.size(), availableIndex + 1));
-            }
-
-        }, this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    public int getAvailableHomeLimit(final User user) {
+    @EventHandler
+    private void onUserTeleportHome(UserTeleportHomeEvent event) {
+        if (event.getHomeType() != UserTeleportHomeEvent.HomeType.HOME) {
+            return;
+        }
+        if (!(event.getUser() instanceof User user)) {
+            return;
+        }
+
+        List<String> homes = user.getHomes();
+        int index = homes.indexOf(event.getHomeName());
+        int availableIndex = getAvailableHomeLimit(user) - 1;
+        if (index <= availableIndex) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        for (int i = 0; i < homes.size(); i++) {
+            if (i > availableIndex) {
+                homes.set(i, ChatColor.STRIKETHROUGH + homes.get(i));
+            }
+        }
+        user.sendMessage(I18n.tl("homes", StringUtil.joinList(homes), homes.size(), availableIndex + 1));
+    }
+
+    private int getAvailableHomeLimit(final User user) {
         ISettings settings = getPlugin(Essentials.class).getSettings();
         int limit = 1;
         if (user.isAuthorized("essentials.home.multiple")) {
